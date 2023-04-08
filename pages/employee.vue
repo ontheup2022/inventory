2ac55b4cb2ebfa8883d61dffcb6b8bb6cecb25f0
ontheup2022 <6331305029@lamduan.mfu.ac.th>
2023-04-08 14:22:08 +0700

    project

--- pages/employee.vue
+++ pages/employee.vue
@@ -1,60 +1,13 @@
-<template>
-  <v-form>
-    <v-container>
-      <v-row>
-        <v-col cols="12" sm="6" md="12">
-          <v-text-field label="รหัสสมาชิก" placeholder="" solo></v-text-field>
-        </v-col>
-
-        <v-col cols="12" sm="6" md="3">
-          <v-text-field label="ชื่อผู้ยืม" placeholder=""></v-text-field>
-        </v-col>
-
-        <v-col cols="12" sm="6" md="3">
-          <v-text-field label="นามสกุล" placeholder=""></v-text-field>
-        </v-col>
-
-        <v-col cols="12" sm="6" md="6">
-          <v-text-field label="วันที่ยืม" solo></v-text-field>
-        </v-col>
-        <v-col cols="12" sm="6" md="3">
-          <v-text-field label="ชื่อผู้รับ" placeholder=""></v-text-field>
-        </v-col>
-
-        <v-col cols="12" sm="6" md="3">
-          <v-text-field label="นามสกุล" placeholder=""></v-text-field>
-        </v-col>
-
-        <v-col cols="12" sm="6" md="6">
-          <v-text-field label="วันที่คืน" placeholder="" solo></v-text-field>
-        </v-col>
-
-        <v-col cols="12" sm="6" md="12">
-          <v-text-field label="ชื่อหนังสือ" filled></v-text-field>
-        </v-col>
-
-        <v-col cols="12" sm="6" md="3">
-          <v-text-field label="ราคาหนังสือ" outlined></v-text-field>
-        </v-col>
-
-        <v-col cols="12" sm="6" md="3">
-          <v-text-field label="หมวดหมู่" placeholder="" outlined></v-text-field>
-        </v-col>
-      </v-row>
-
-      <v-btn depressed color="primary"> ตกลง </v-btn>
-      <v-btn depressed color="error"> ยกเลิก </v-btn>
-    </v-container>
-  </v-form>
-</template>
+<!-- <template>
+</template> -->
     
     
     
   
   <script>
-export default {
-  name: "InspirePage",
-};
+// export default {
+//   name: "InspirePage",
+// };
 </script>
   
   
