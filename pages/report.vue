98d530fae4f29fccfbc4b4ae699f672af93df292
ontheup2022 <6331305029@lamduan.mfu.ac.th>
2023-04-12 18:37:02 +0700

    live

---
+++ pages/report.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row justify="center" align="baseline">
+    <v-col cols="36" md="24" sm="12" xl="36"> </v-col>
+
+    <v-col cols="36" md="36" sm="12">
+      <v-btn
+        color="primary"
+        depressed
+        elevation="36"
+        large
+        x-large
+        nuxt
+        to="/historybuyapart"
+      >
+        การแจ้งเตือน
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+</script>
