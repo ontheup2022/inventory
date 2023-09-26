3b8a1fd1ce84fdda22c461915334292fbdb02590
ontheup2022 <6331305029@lamduan.mfu.ac.th>
2023-09-27 02:12:25 +0700

    edit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
+  const functions = require('firebase-functions');
+
+  exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+      .onCreate((snap, context) => {
+        const original = snap.data().original;
+        console.log('Uppercasing', context.params.documentId, original);
+        const uppercase = original.toUpperCase();
+        return snap.ref.set({uppercase}, {merge: true});
+      });
+  
 
 const routerOptions = [
   {
